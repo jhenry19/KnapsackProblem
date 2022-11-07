2a7328c4b03a460a167369562864e50e19a5082d
jhenry19 <jhny277@gmail.com>
2022-11-07 16:52:47 -0500

    Initialized memoization table and created printing function for it

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -6,9 +6,30 @@ import java.util.ArrayList;
 public class Knapsack {
 	ArrayList<Item> items;
 	int weight;
+	int[][] M; // Memoization table
+	int n; // the number of items given in the problem
 
 	Knapsack(ArrayList<Item> problemItems, int w) {
 		items = problemItems;
 		weight = w;
+		n = items.size();
+		M = new int[n][w]; // initialize the memoization table for this problem
+	}
+
+	int runProblem() {
+		for (int i = 0; i < n; i++) {
+			M[0][i] = 0;
+		}
+		printMTable();
+		return 1;
+	}
+
+	void printMTable() {
+		for (int i = 0; i < n; i++){
+			for (int z = 0; z < weight; z++) {
+				System.out.print(M[i][z] + " ");
+			}
+				System.out.println();
+		}
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
+		ArrayList<Item> items = new ArrayList<>();
+		items.add(new Item(1, 1, 1));
+		items.add(new Item(2, 6, 2));
+		items.add(new Item(3, 18, 5));
+		items.add(new Item(4, 22, 6));
+		items.add(new Item(5, 28, 7));
 
+		Knapsack knapsack = new Knapsack(items, 10);
+		knapsack.runProblem();
 	}
 }
