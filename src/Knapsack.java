cd356eac3b56e985ca05a5a76929d541d1c93776
jhenry19 <jhny277@gmail.com>
2022-11-08 11:58:50 -0500

    finished adding comments

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,4 +1,3 @@
-import javax.sound.midi.SysexMessage;
 import java.util.ArrayList;
 
 /**
@@ -16,13 +15,18 @@ public class Knapsack {
 		n = items.size();
 		M = new int[n + 1][w + 1]; // initialize the memoization table for this problem
 
+		// Prints the solution to the console
 		runProblem();
 		System.out.print("The following items are included in the optimal solution: ");
 		findSolution(n, w);
 		System.out.println("The total value of the Knapsack is " + M[n][weight]);
 	}
 
-	int runProblem() {
+	/**
+	 * Populates the memoization table. Runs in in O(n) time.
+	 */
+	void runProblem() {
+		// Populate and print the first row of the table
 		for (int z = 0; z <= n; z++) {
 			M[0][z] = 0;
 		}
@@ -31,12 +35,10 @@ public class Knapsack {
 		int itemValue, itemWeight;
 
 		for (int i = 1; i <= n; i++) {
-			// Information about the item in question is saved for ease of use.
 			// (i-1) because item indices start at 1 while the matrix indices start at 0
 			itemWeight = items.get(i - 1).weight;
 			itemValue = items.get(i - 1).value;
 			for (int w = 0; w <= weight; w++) {
-
 				if (itemWeight > w) { // item i is NOT part of the optimal solution
 					M[i][w] = M[i-1][w];
 				}
@@ -46,24 +48,32 @@ public class Knapsack {
 			}
 			printMTable(i);
 		}
-		return M[n-1][weight-1];
 	}
 
+	/**
+	 * Find the items that make up a possible optimal solution.
+	 */
 	void findSolution(int j, int recursiveWeight) {
-		if (j == 0 || recursiveWeight == 0) System.out.println("");
+		if (j == 0 || recursiveWeight == 0) System.out.println(""); // If done looking at items or weight capacity met
 		else {
+			// Variables used for readability and eas of use
 			int itemValue = items.get(j - 1).value;
 			int itemWeight = items.get(j - 1).weight;
 
+			// If the items weight will lead to an overweight bag, it is not part of the optimal solution
 			if (recursiveWeight - itemWeight < 0) findSolution(j - 1, recursiveWeight);
+			// If the item is part of the optimal solution
 			else if (itemValue + M[j - 1][recursiveWeight - itemWeight] >= M[j - 1][weight]) {
-				System.out.print("item " + Integer.toString(j) + " ");
+				System.out.print("item " + j + " ");
 				findSolution(j - 1, recursiveWeight - itemWeight);
-			} else
+			} else // if the items is not part of the optimal solution
 				findSolution(j - 1, recursiveWeight);
 		}
 	}
 
+	/**
+	 * Prints the whole memoization table
+	 */
 	void printMTable() {
 		for (int i = 0; i <= n; i++){
 			for (int z = 0; z <= weight; z++) {
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
+		// Create list of items in knapsack problem (must in in increasing order of value)
 		ArrayList<Item> items = new ArrayList<>();
 		items.add(new Item(1, 1, 1));
 		items.add(new Item(2, 6, 2));
@@ -9,6 +10,6 @@ public class Main {
 		items.add(new Item(4, 22, 6));
 		items.add(new Item(5, 28, 7));
 
-		Knapsack knapsack = new Knapsack(items, 11);
+		Knapsack knapsack = new Knapsack(items, 10);
 	}
 }
